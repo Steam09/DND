96b21cfabc766d9f3cddeb52d34423f5e0ce0dd7
Trison Li <trisonli0809@gmail.com>
2025-01-18 14:57:40 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('command', (data) => {
+        console.log('Command received:', data);
+        // Handle the command and update game state
+        // Broadcast the updated state to all clients
+        io.emit('update', { /* updated game state */ });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
