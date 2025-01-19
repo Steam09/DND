372ec89dd67747366c11571450ed278c27007765
Trison Li <trisonli0809@gmail.com>
2025-01-18 22:24:50 -0500

    Communication between single-player and DM
    Signed-off-by: Trison Li <trisonli0809@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-const db = require('./db');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.static('public'));
 
@@ -14,9 +18,7 @@ io.on('connection', (socket) => {
 
     socket.on('command', (data) => {
         console.log('Command received:', data);
-        handleCommand(data, (response) => {
-            io.emit('update', response);
-        });
+        io.emit('update', data); // Broadcast the update to all clients
     });
 
     socket.on('disconnect', () => {
@@ -26,23 +28,3 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-function handleCommand(data, callback) {
-    if (data.action === 'attack') {
-        db.run("UPDATE players SET health = health - 10 WHERE username = ?", [data.username], function(err) {
-            if (err) {
-                return console.error(err.message);
-            }
-            callback({ message: 'Player attacked!', health: this.changes });
-        });
-    } else if (data.action === 'defend') {
-        db.run("UPDATE players SET health = health + 5 WHERE username = ?", [data.username], function(err) {
-            if (err) {
-                return console.error(err.message);
-            }
-            callback({ message: 'Player defended!', health: this.changes });
-        });
-    } else {
-        callback({ message: `Command ${data.action} executed!` });
-    }
-}
